764619403c0794a96700e0eed4ade0227e327822
buihaiduongdev <haiduongtinhoc@gmail.com>
2025-10-03 15:07:05 +0700

    done demo1

---
+++ src/main/java/com/example/bth11/Controller/CustomerController.java
@@ -0,0 +1,47 @@
+package com.example.bth11.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bth11.entity.Customer;
+
+@RestController
+public class CustomerController {
+	final private List<Customer> customers = List.of(
+			Customer.builder().id("001").name("Hai Duong").email("haiduong@gmail.comcom").build(),
+			Customer.builder().id("002").name("Tam").email("Tam@mail.com").build());
+
+	@GetMapping("/hello")
+	public ResponseEntity<String> hello() {
+		String html = """
+				<html>
+				    <body>
+				        <h1>Hello is Guest</h1>
+				        <form action="/logout" method="post">
+				            <button type="submit">Logout</button>
+				        </form>
+				    </body>
+				</html>
+				""";
+		return ResponseEntity.ok().header("Content-Type", "text/html").body(html);
+	}
+
+	@GetMapping("/customer/all")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public ResponseEntity<List<Customer>> getCustomersList() {
+		List<Customer> list = this.customers;
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping("/customer/{id}")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN') or hasAuthority('ROLE_USER')")
+	public ResponseEntity<Customer> getCustomerList(@PathVariable("id") String id) {
+		List<Customer> customers = this.customers.stream().filter(customer -> customer.getId().equals(id)).toList();
+		return ResponseEntity.ok(customers.get(0));
+	}
+}
---
+++ src/main/java/com/example/bth11/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.bth11.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+
+public class SecurityConfig {
+
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		UserDetails admin = User.withUsername("duong").password(encoder.encode("123")).roles("ADMIN").build();
+		UserDetails user = User.withUsername("user").password(encoder.encode("123")).roles("USER").build();
+		return new InMemoryUserDetailsManager(admin, user);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/hello").authenticated() // Hoặc permitAll() theo
+																								// slide trang 23
+						.requestMatchers("/customer/**").authenticated().anyRequest().denyAll())
+				.formLogin(Customizer.withDefaults()).build();
+	}
+
+}
